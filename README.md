926bbad41d576b2832a32fb1200fb94e5939047b
peyo <peteryyoon@gmail.com>
2020-06-11 20:24:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dsa-stack-and-queue
---
+++ queues/Queue.js
@@ -0,0 +1,40 @@
+const _Node = require("./Node");
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  };
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if (this.last) {
+      this.last.next = node;
+    }
+
+    //make the new node the last item on the queue
+    this.last = node;
+  };
+
+  dequeue() {
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = this.first.next;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
+  }
+}
+
+module.exports = Queue
